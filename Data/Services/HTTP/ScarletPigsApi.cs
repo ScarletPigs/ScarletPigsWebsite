5cf61ee4c69cc7a33746ac84ee11ec5badcd7d2c
Rasmus Tanggaard <rasmus@tthost.dk>
2024-06-25 16:58:14 +0200

    Added basic api service to fetch events for calendar

---
+++ Data/Services/HTTP/ScarletPigsApi.cs
@@ -0,0 +1,24 @@
+﻿using ScarletPigsWebsite.Data.Models.Events;
+
+namespace ScarletPigsWebsite.Data.Services.HTTP
+{
+    public interface IScarletPigsApi
+    {
+        public Task<List<Event>> GetEventsAsync();
+    }
+
+    public class ScarletPigsApi : IScarletPigsApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public ScarletPigsApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Event>> GetEventsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Event>>("events") ?? new List<Event>();
+        }
+    }
+}
